ec216f992562ffe5889830df53a1e35dea53344b
kookkig11 <kookkig2542@gmail.com>
2019-08-13 13:13:16 +0700

    add class file testing

--- classTesting/Car.php
+++ classTesting/Car.php
@@ -1,16 +1,48 @@
 <?php
-    class Car {}
-    
+    class Car {
+        var $color;
+        var $brand;
+
+        function __construct($color, $brand){
+            $this->color = $color;
+            $this->brand = $brand;
+        }
+
+        function setColor($color) {
+            $this->color = $color;
+        }
+
+        function setBrand(){
+            $this->brand = $brand;    
+        }
+
+        function getColor(){
+            return $this->color;
+        }
+
+        
+        function getBrand(){
+            return $this->brand;
+        }
+    }
+    /*
     $car1 = new Car();
     $car2 = new Car;
     // class ไม่จำเป็นต้องมี () ในการเรียกก็ได้
+    */
+    
+    $car1 = new Car("Black", "Ford");
+    $car2 = new Car("Green", "Toyota");
 
     var_dump($car1);
+    echo "\n";
     var_dump($car2);
+    echo "\n";
 
-    // output:
-    // object(Car)#1 (0) {
-    // }
-    // object(Car)#2 (0) {
-    // }
+    var_dump($car1->getColor());
+    var_dump($car1->getBrand());
+    echo "\n";
+
+    echo $car1->getColor() . "\n";
+    echo $car2->getColor() . "\n";
 ?>
